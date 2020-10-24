dbb521b168e0a16d0832cb0b48fbe77a35e4691b
Diogo Lindoso <diogolindoso@gmail.com>
2020-10-24 11:14:36 -0400

    Adicionado campo cpf e recuperado dados para form editar

--- resources/views/pessoas/form.blade.php
+++ resources/views/pessoas/form.blade.php
@@ -22,23 +22,30 @@
         @csrf
         <div class="form-group">
           <label for="nome">Nome</label>
-          <input type="text" class="form-control" id="nome" aria-describedby="nome" name="nome">
+        <input type="text" class="form-control" id="nome" aria-describedby="nome" name="nome" value="{{$pessoa->nome ?? ''}}">
           @error('nome')
             <small class="text-danger">{{$message}}</small> 
           @enderror
         </div>
         <div class="form-group">
           <label for="telefone">Telefone</label>
-          <input type="text" class="form-control" id="telefone" name="telefone">
+          <input type="text" class="form-control" id="telefone" name="telefone" value="{{$pessoa->telefone ?? ''}}">
           @error('telefone')
           <small class="text-danger">{{$message}}</small> 
         @enderror
         </div>
         <div class="form-group">
             <label for="exampleInputEmail1">Email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value="{{$pessoa->email ?? ''}}">
             {{-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --}}
-          </div>
+        </div>
+        <div class="form-group">
+          <label for="cpf">CPF</label>
+          <input type="text" class="form-control" id="cpf" name="cpf" value="{{$pessoa->cpf ?? ''}}">
+          @error('cpf')
+          <small class="text-danger">{{$message}}</small> 
+        @enderror
+        </div>
         <button type="submit" class="btn btn-primary">Salvar</button>
       </form>
 
